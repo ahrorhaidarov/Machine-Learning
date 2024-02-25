37ed8a7730f4a7a3028f7c9e2fa12ba208d89862
ahrorhaidarov <alanjon1312@gmail.com>
2024-02-25 20:40:01 +0500

    Kmeans clustering added

---
+++ Clustering/K-Means-Clustering/implementation.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class KMeansClustering:
+    def __init__(self, k=3):
+        self.k = k
+        self.centroids = None
+
+    @staticmethod
+    def euclidean_distance(data_point, centroids):
+        return np.sqrt(np.sum((centroids - data_point) ** 2, axis=1))
+
+    def fit(self, X, max_iteration=200):
+        self.centroids = np.random.uniform(np.amin(X, axis=0), np.amax(X, axis=0),
+                                           size=(self.k, X.shape[1]))
+
+        for _ in range(max_iteration):
+            y = []
+
+            for data_point in X:
+                distances = KMeansClustering.euclidean_distance(data_point, self.centroids)
+                clusetr_num = np.argmin(distances)
+                y.append(clusetr_num)
+
+            y = np.array(y)
+
+            cluster_indices = []
+
+            for i in range(self.k):
+                cluster_indices.append(np.argwhere(y == i))
+
+            cluster_centers = []
+
+            for i, indices in enumerate(cluster_indices):
+                if len(indices) == 0:
+                    cluster_centers.append(self.centroids[i])
+                else:
+                    cluster_centers.append(np.mean(X[indices], axis=0)[0])
+
+            if np.max(self.centroids - np.array(cluster_centers)) < 0.0001:
+                break
+            else:
+                self.centroids = np.array(cluster_centers)
+        return y
+
